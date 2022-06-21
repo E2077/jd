f3ffbec34b43523c9d53852ef423344ba141c117
E2077 <88936201+E2077@users.noreply.github.com>
2022-06-21 15:34:51 +0800

    Update 拉取E2077.sh

--- 拉取E2077.sh
+++ 拉取E2077.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
-
-# shellcheck disable=SC2005,2188
-<<'COMMENT'
-cron: 7 7 7 7 7
-new Env('拉取E2077');
-COMMENT
-
-
-ql repo https://github.com/E2077/jd.git "" "" "^jd[^_]|USER|utils|function|sign|sendNotify|ql|JDJR" ""
+#cron: 7 7 7 7 7
+#new Env('拉取E2077');
+echo "开始拉库"
+ql repo https://github.com/E2077/jd.git "" "" "^jd[^_]|USER|utils|function|sign|sendNotify|ql|JDJR"
