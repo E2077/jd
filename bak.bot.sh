a1a9c540e1870643be1c226c24b7a782edc7d985
E2077 <88936201+E2077@users.noreply.github.com>
2022-08-02 10:15:48 +0800

    Add files via upload

--- bak.bot.sh
+++ bak.bot.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 #cron: 7 7 7 7 7
 #new Env('备份机器人配置');
+if [ ! -d "/ql/data//bot备份" ];then
+mkdir "/ql/data//bot备份"
+echo "创建文件夹成功"
+else
+echo "文件夹已经存在"
+fi
 cp  /ql/data/config/bot.json /ql/data//bot备份/bot.json
 cp  /ql/data/config/diybotset.json /ql/data//bot备份/diybotset.json
 cp  /ql/data/config/jk.json /ql/data//bot备份/jk.json
