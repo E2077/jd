e977cbb9ef9ac957ca8be06eff545f16d680aa43
E2077 <88936201+E2077@users.noreply.github.com>
2022-08-02 10:22:21 +0800

    Add files via upload

--- bak.bot.sh
+++ bak.bot.sh
@@ -3,10 +3,11 @@
 #new Env('备份机器人配置');
 if [ ! -d "/ql/data//bot备份" ];then
 mkdir "/ql/data//bot备份"
-echo "创建文件夹成功"
+echo "备份文件夹不存在,创建备份文件夹成功"
 else
-echo "文件夹已经存在"
+echo "备份文件夹已经存在,开始备份"
 fi
 cp  /ql/data/config/bot.json /ql/data//bot备份/bot.json
 cp  /ql/data/config/diybotset.json /ql/data//bot备份/diybotset.json
 cp  /ql/data/config/jk.json /ql/data//bot备份/jk.json
+echo "备份完成,路径 /ql/data//bot备份"
